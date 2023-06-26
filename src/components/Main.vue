d79939f44e20494fd71bd8867aa2eae23268b850
pierdomenicoguarnieri <pierdo.guarnieri@gmail.com>
2023-06-26 21:26:58 +0200

    add: markup and store.js

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -1,15 +1,55 @@
 <script>
-  export default {
-    name: 'Main'
+import ProjectCard from './partials/ProjectCard.vue';
+import axios from 'axios';
+import {store} from '../data/store';
+
+export default {
+  name: 'Main',
+
+  components: {
+    ProjectCard
+  },
+
+  data(){
+    return{
+      store,
+      projects: null,
+    }
+  },
+
+  methods:{
+    getApi(endpoint){
+      axios.get(endpoint).then(results => {
+        this.projects = results.data.data;
+        console.log(this.projects);
+      })
+    }
+  },
+
+  mounted(){
+    this.getApi(store.apiUrl)
   }
+}
 </script>
 
 <template>
   <main>
-    <h1>Main</h1>
+    <div class="container">
+      <h1>Main</h1>
+      <div class="row row-cols-5 py-3">
+        <ProjectCard v-for="project in projects" :key="project.id" :title="project.title" :type="project.type.name" :technologies="project.technologies" :finished="project.finished"/>
+      </div>
+    </div>
   </main>
 </template>
 
 <style lang="scss">
-
+  main{
+    height: 100vh;
+    background-color: lighten($color: #000000, $amount: 15%);
+    color: white;
+    .container{
+      padding: 3.5rem 0;
+    }
+  }
 </style>
---
+++ src/components/partials/ProjectCard.vue
@@ -0,0 +1,37 @@
+<script>
+export default {
+  name: 'ProjectCard',
+
+  props:{
+    title: String,
+    type: String,
+    technologies: Array,
+    finished: Boolean
+  }
+}
+</script>
+
+<template>
+  <div class="col mb-4">
+    <div class="card h-100">
+      <div class="card-body d-flex flex-column justify-content-between">
+        <h5 class="card-title">{{title}}</h5>
+        <div class="card-subtitle mb-2 text-body-secondary">
+          <span class="badge text-bg-primary">{{ type }}</span>
+        </div>
+
+        <div>
+          <span>Tecnologie: </span>
+          <span v-for="technology in technologies" :key="technology.id" class="badge text-bg-warning me-2">{{ technology.name }}</span>
+          <span class="badge text-bg-danger me-2" v-if="!technologies.length">No technologies</span>
+        </div>
+        <p class="card-text">Finished: <i class="fa-solid fa-check text-success" v-show="finished"></i>
+            <i class="fa-solid fa-x text-danger" v-show="!finished"></i></p>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+
+</style>
